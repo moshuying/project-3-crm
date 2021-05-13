ca08f82258a8b98ab3f3870a2dd2da6e62109304
moshuying <1460083332@qq.com>
2021-05-13 10:35:48 +0800

    完善返回DO

--- back/src/main/java/com/zoctan/seedling/dto/LoginResultDTO.java
+++ back/src/main/java/com/zoctan/seedling/dto/LoginResultDTO.java
@@ -8,8 +8,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.io.Serializable;
 import java.util.*;
 
@@ -27,12 +25,11 @@ public class LoginResultDTO extends AbstractConverter<LoginResultDTO, LoginResul
     @Schema(name = "过期时间")
     private String expireAt = new Date().toString();
 
-    private Map permissions = new HashMap<>();
-    private List roles = new ArrayList<Object>();
-    private Map position = new HashMap<>();
+    private List<Object> permissions = new ArrayList<>();
+    private List<Object> roles = new ArrayList<>();
 
     @Schema(name = "用户信息")
-    private Map user = new HashMap<>();
+    private Map<String, Object> user = new HashMap<>();
 
     public void setUserName(String name) {
         this.getUser().put("name", name);
@@ -40,15 +37,18 @@ public class LoginResultDTO extends AbstractConverter<LoginResultDTO, LoginResul
 
     public LoginResultDTO() {
 //        注入虚拟数据
-        this.position.put("CN", "前端工程师 | 蚂蚁金服-计算服务事业群-REACT平台");
-        this.position.put("HK", "前端工程師 | 螞蟻金服-計算服務事業群-REACT平台");
-        this.position.put("US", "Front-end engineer | Ant Financial - Computing services business group - REACT platform");
-
-        this.permissions.put("id", "queryForm");
-        this.permissions.put("operation", new String[]{"add", "edit"});
+        Map<String, Object> permissions = new HashMap<>();
+        permissions.put("id", "queryForm");
+        permissions.put("operation", new String[]{"add", "edit"});
+        this.permissions.add(permissions);
 
         this.user.put("address", "贺州市");
         this.user.put("avatar", "https://gw.alipayobjects.com/zos/rmsportal/WhxKECPNujWoWEFNdnJE.png");
+        Map<String, Object> position = new HashMap<>();
+        position.put("CN", "前端工程师 | 蚂蚁金服-计算服务事业群-REACT平台");
+        position.put("HK", "前端工程師 | 螞蟻金服-計算服務事業群-REACT平台");
+        position.put("US", "Front-end engineer | Ant Financial - Computing services business group - REACT platform");
+        this.user.put("position", position);
     }
 
     @Override
