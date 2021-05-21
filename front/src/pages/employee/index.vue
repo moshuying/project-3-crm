dde03f2a8efcd1284527a3422af99e05b7cb37c4
moshuying <1460083332@qq.com>
2021-05-21 12:03:58 +0800

    补充员工界面logo

--- front/src/pages/employee/index.vue
+++ front/src/pages/employee/index.vue
@@ -37,7 +37,7 @@
               cancel-text="否"
               @confirm="exportFile()"
           >
-            <a-button type="primary">导出</a-button>
+            <a-button icon="download" type="primary">导出</a-button>
           </a-popconfirm>
           <a-upload :showUploadList="false" :file-list="fileList" :remove="handleRemove" :before-upload="beforeUpload">
             <a-button> <a-icon type="upload" /> 导入数据</a-button>
