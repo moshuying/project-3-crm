4b617b0e6f1c404ae521478c709c3c6c7cb7ce66
moshuying <1460083332@qq.com>
2021-05-22 18:52:19 +0800

    后台完善最后一个功能，统计分析

---
+++ back/src/main/java/com/msy/plus/controller/AnalysisController.java
@@ -0,0 +1,39 @@
+package com.msy.plus.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.msy.plus.core.response.Result;
+import com.msy.plus.core.response.ResultGenerator;
+import com.msy.plus.dto.AnalysisQuery;
+import com.msy.plus.entity.Analysis;
+import com.msy.plus.entity.CFUHSearch;
+import com.msy.plus.service.CustomerManagerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.annotations.Api;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @author MoShuYing
+ * @date 2021/05/15
+ */
+@PreAuthorize("hasAuthority('ADMIN')")
+@Api(tags={"统计分析接口"})
+@RestController
+@RequestMapping("/analysis")
+public class AnalysisController {
+    @Resource
+    CustomerManagerService customerManagerService;
+
+    @Operation(description = "统计分析")
+    @PostMapping
+    public Result listAndSearch(@RequestBody AnalysisQuery analysisQuery) {
+        PageHelper.startPage(analysisQuery.getPage(),analysisQuery.getSize());
+        PageInfo<Analysis> pageInfo = PageInfo.of(customerManagerService.queryAnalysis(analysisQuery));
+        return ResultGenerator.genOkResult(pageInfo);
+    }
+
+}
---
+++ back/src/main/java/com/msy/plus/dto/AnalysisQuery.java
@@ -0,0 +1,30 @@
+package com.msy.plus.dto;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AnalysisQuery {
+    private Integer page=1;
+    private Integer size=10;
+    private String name="";
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date startTime;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date endTime;
+    /**
+     * groupType
+     * 1 员工
+     * 2 年
+     * 3 月
+     * 4 日
+     */
+    private int groupType;
+}
