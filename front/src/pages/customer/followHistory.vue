0d91e9706a5bb1c0703a4eae3fa220b244e623c9
moshuying <1460083332@qq.com>
2021-05-22 16:01:11 +0800

    完善客户跟进历史的所有功能

--- front/src/pages/customer/followHistory.vue
+++ front/src/pages/customer/followHistory.vue
@@ -48,39 +48,79 @@
             @change="handleTableChange"
         >
            <span slot="action" slot-scope="text">
-              <a @click="updateItem(text.id)">编辑</a>
+              <a @click="showFollowModal(text.id,text)">编辑</a>
            </span>
         </a-table>
       </div>
     </a-card>
+    <!--    跟进-->
     <a-modal
-        :title="title"
-        :visible="visible"
-        :confirm-loading="confirmLoading"
-        @ok="handleOk"
-        @cancel="handleCancel"
-        okText="提交"
-    >
-      <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        title="跟进记录"
+        :visible="followVisible"
+        :confirm-loading="followConfirmLoading"
+        @ok="handleFollowOk"
+        @cancel="handleFollowCancel"
+        okText="保存">
+      <a-form :form="followForm" :layout="`horizontal`">
         <a-form-item hidden>
           <a-input v-decorator="['id',{ rules: [{ required: false}] }]"/>
         </a-form-item>
-        <a-form-item label="数据字典名称">
+        <a-form-item disabled label="客户姓名">
           <a-input
-              v-decorator="['title', { rules: [{ required: true, message: '请输入数据字典名称' }] }]"
+              v-decorator="['name', { rules: [{ required: true, message: '姓名'  }]}]"
+              disabled />
+        </a-form-item>
+        <a-form-item disabled label="跟进时间">
+          <a-date-picker
+              @change="followTransTime"
+              show-time
+              @ok="onOk"
+              v-decorator="['tracetime', { rules: [{ required: true, message: '跟进时间'  }]}]"
+          />
+        </a-form-item><a-form-item disabled label="跟进内容">
+        <a-input
+            v-decorator="['tracedetails', { rules: [{ required: true, message: '跟进内容'  }]}]"
+        />
+      </a-form-item>
+        <a-form-item disabled label="跟进方式">
+          <a-select
+              v-decorator="['tracetype', { rules: [{ required: true, message: '跟进方式'  }]}]">
+            <a-select-option
+                :value="value.id"
+                :key="index"
+                v-for="(value,key,index) in dictionaryDetailsFollow">
+              {{value.title}}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item disabled label="跟进结果">
+          <a-select
+              v-decorator="['traceresult', { rules: [{ required: true, message: '跟进结果'  }]}]">
+            <a-select-option
+                :value="key"
+                :key="index"
+                v-for="(value,key,index) in followTraceResult">
+              {{value}}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item disabled label="备注">
+          <a-textarea
+              v-decorator="['comment', { rules: [{ required: true, message: '备注'  }]}]"
+              :auto-size="{ minRows: 3, maxRows: 5 }"
           />
         </a-form-item>
-        <a-form-item label="数据字典编号">
-          <a-input
-              v-decorator="['sn',{ rules: [{ required: true, message: '请输入数据字典编号' }] },]"
-              placeholder="请输入数据字典编号"
-          />
-        </a-form-item>
-        <a-form-item label="数据字典简介">
-          <a-input
-              v-decorator="['intro',{ rules: [{ required: true, message: '请输入数据字典简介' }] },]"
-              placeholder="请输入数据字典简介"
-          />
+        <a-form-item disabled label="跟进类型">
+          <a-select
+              v-decorator="['type', { rules: [{ required: true, message: '跟进类型'  }]}]"
+          >
+            <a-select-option
+                :value="key"
+                :key="index"
+                v-for="(value,key,index) in followType">
+              {{value}}
+            </a-select-option>
+          </a-select>
         </a-form-item>
       </a-form>
     </a-modal>
@@ -146,12 +186,12 @@ export default {
       selectedRows: [],
       pagination: {},
       loading: false,
-      // modal
-      title: '添加',
-      visible: false,
-      confirmLoading: false,
-      // modal form
-      form: this.$form.createForm(this, {name: 'coordinated'}),
+      // 跟进
+      followForm:this.$form.createForm(this, {name: 'coordinated'}),
+      followVisible :false,
+      followConfirmLoading :false,
+      followType:customerFollowUpHistory.type,
+      followTraceResult:customerFollowUpHistory.traceresult,
       dictionaryDetailsFollow:[],
       // 员工表
       employeeList:[]
@@ -226,43 +266,58 @@ export default {
         this.form.setFieldsValue({"intro": data.data["intro"]})
       })
     },
-    // modal
-    showModal(title = '添加') {
-      this.visible = true;
-      this.title = title || '添加'
-      this.form.resetFields()
+    // 移交
+    handleFollowCancel(){
+      this.followVisible = false;
+      this.followConfirmLoading = false
+      this.followForm.resetFields()
     },
-    handleOk() {
-      this.confirmLoading = true;
-      this.form.validateFields((err, values) => {
-
+    handleFollowOk(){
+      this.followConfirmLoading = true;
+      this.followForm.validateFields((err, values) => {
         if (err) {
           console.log("form error");
+          this.followConfirmLoading = false
           return;
         }
+        values.tracetime = values.tracetime.toDate()
+
         customerFollowUpHistory['update'](values).then(({data}) => {
-          this.confirmLoading = false;
+          this.followConfirmLoading = false;
           if (data.code !== 200) {
             this.$notification['error']({
-              message: this.title + '数据字典信息出现错误',
+              message:'跟进记录出现错误',
               description: '建议检查网络连接或重新登陆',
             });
           }else{
             this.$notification.success({
-              message: this.title + '成功',
-              description: this.title + '数据字典信息成功',
+              message:'成功',
+              description:'跟进记录成功',
             });
           }
-          this.visible = false
-          this.fetch({"page": this.pagination.current, "size": 10})
+          this.followVisible = false
+          this.query()
         })
       });
     },
-    handleCancel() {
-      this.visible = false;
-      this.title=''
-      this.confirmLoading = false
-      this.form.resetFields()
+    async showFollowModal(id,line){
+      this.followVisible = true;
+      this.followConfirmLoading = false
+      await this.followForm.resetFields()
+      // 这里不能循环
+      this.$nextTick(()=>{
+        this.followForm.setFieldsValue({id})
+        this.followForm.setFieldsValue({name:line.name})
+        this.followForm.setFieldsValue({comment:line.comment})
+        this.followForm.setFieldsValue({tracedetails:line.tracedetails})
+        this.followForm.setFieldsValue({tracetime:new moment(new Date())})
+        this.followForm.setFieldsValue({tracetype:this.dictionaryDetailsFollow[0].id})
+        this.followForm.setFieldsValue({type:Object.keys(this.followType)[0]})
+        this.followForm.setFieldsValue({traceresult:Object.keys(this.followTraceResult)[0]})
+      })
+    },
+    followTransTime(date, dateString) {
+      console.log(date, dateString);
     },
     onChange(value, dateString) {
       console.log('Selected Time: ', value);
