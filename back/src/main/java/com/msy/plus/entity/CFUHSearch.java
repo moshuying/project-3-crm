3bacabce61ecf42aeeffe83f1c0395fd52a94773
moshuying <1460083332@qq.com>
2021-05-22 12:08:39 +0800

    后台完善客户历史列表查询

---
+++ back/src/main/java/com/msy/plus/entity/CFUHSearch.java
@@ -0,0 +1,13 @@
+package com.msy.plus.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 客户跟进历史分页查询搜索
+ */
+@Getter
+@Setter
+public class CFUHSearch extends CustomerFollowUpHistory{
+    private String name;
+}
