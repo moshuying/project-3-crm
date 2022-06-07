12cbdf1baf17d495c212e4cc7d05340b5e700f69
墨抒颖 <37231523+moshuying@users.noreply.github.com>
2022-06-07 14:33:32 +0800

    Update resetDB.sh

--- back/resetDB.sh
+++ back/resetDB.sh
@@ -6,4 +6,5 @@ while IFS= read -r -d '' sql; do
   echo "$sql"" -> "$db
   mysql -uroot -proot $db <"$sql"
 done < <(find src/test/resources/sql/dev/ -name '*.sql' -print0)
+echo "finished"
 echo "import $db done"
