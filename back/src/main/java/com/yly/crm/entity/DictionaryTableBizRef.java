2894953201e466f5ccc194a6b46af24fd8b0666d
xm <xm@yliyun.com>
2023-06-02 19:21:58 +0800

    updata  some

--- back/src/main/java/com/yly/crm/entity/DictionaryTableBizRef.java
+++ back/src/main/java/com/yly/crm/entity/DictionaryTableBizRef.java
@@ -21,64 +21,20 @@ public class DictionaryTableBizRef implements Serializable {
     private Integer id;
 
     /**
-     * 字典的id
-     */
-    private Integer dicId;
-
-    /**
      * 业务设置表：1、企业表，2、联系人表，3、员工表、4跟进表、5、业务表、6其他
      */
-    private Integer bizName;
+    private String bizName;
 
     /**
      * 数据库表名称
      */
     private String tableName;
 
+    /**
+     * 主要是字典的名字与id的数组{dic_id:2, dic_name:type}多个数字组成
+     */
+    private String schemaJson;
+
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        DictionaryTableBizRef other = (DictionaryTableBizRef) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getDicId() == null ? other.getDicId() == null : this.getDicId().equals(other.getDicId()))
-            && (this.getBizName() == null ? other.getBizName() == null : this.getBizName().equals(other.getBizName()))
-            && (this.getTableName() == null ? other.getTableName() == null : this.getTableName().equals(other.getTableName()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getDicId() == null) ? 0 : getDicId().hashCode());
-        result = prime * result + ((getBizName() == null) ? 0 : getBizName().hashCode());
-        result = prime * result + ((getTableName() == null) ? 0 : getTableName().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", dicId=").append(dicId);
-        sb.append(", bizName=").append(bizName);
-        sb.append(", tableName=").append(tableName);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
