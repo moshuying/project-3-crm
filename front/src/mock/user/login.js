d9748737bcfad03bc19d1a3214013b74554dbb0d
webglfast <91885700+webglfast@users.noreply.github.com>
2023-06-03 08:54:46 +0800

    Update login.js

--- front/src/mock/user/login.js
+++ front/src/mock/user/login.js
@@ -17,7 +17,7 @@ Mock.mock(`${process.env.VUE_APP_API_BASE_URL}/login`, 'post', ({body}) => {
         success = true
         result.data.permissions = [{id: 'queryForm', operation: ['add', 'edit']}]
         result.data.roles = [{id: 'admin', operation: ['add', 'edit', 'delete']}]
-    } else if (name === 'test' || password === '888888') {
+    } else if (name === 'user' || password === '888888') {
         success = true
         result.data.permissions = [{id: 'queryForm', operation: ['add', 'edit']}]
         result.data.roles = [{id: 'test', operation: ['add', 'edit', 'delete']}]
